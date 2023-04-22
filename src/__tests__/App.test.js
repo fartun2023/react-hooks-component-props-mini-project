98d388b39195c75bba9966999f81898c8d3b76ca
fartun <fartunwario34@gmail.com>
2023-04-22 14:21:31 +0300

    competed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,34 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import App from "../components/App";
+import ArticleList from "../components/ArticleList";
 
-test("renders the correct child components", () => {
-  const { container } = render(<App />);
-  expect(container.querySelector(".App")).toBeInTheDocument();
-  expect(container.querySelector(".App header")).toBeInTheDocument();
-  expect(container.querySelector(".App aside")).toBeInTheDocument();
-  expect(container.querySelector(".App main")).toBeInTheDocument();
+const posts = [
+  {
+    id: 1,
+    title: "Components 101",
+    date: "December 15, 2020",
+    preview: "Setting up the building blocks of your site",
+  },
+  {
+    id: 2,
+    title: "React Data Flow",
+    date: "December 11, 2020",
+    preview: "Passing props is never passé",
+  },
+  {
+    id: 3,
+    title: "Function Components vs Class Components",
+    date: "December 10, 2020",
+    preview: "React, meet OOJS.",
+  },
+];
+
+test("renders a <main> element", () => {
+  const { container } = render(<ArticleList posts={posts} />);
+  expect(container.querySelector("main")).toBeInTheDocument();
+});
+
+test("renders an Article component for each post passed as a prop", () => {
+  const { container } = render(<ArticleList posts={posts} />);
+  expect(container.querySelector("main").children).toHaveLength(3);
 });
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a simple blog app created with React.</p>
+      <p>It demonstrates how to use component composition and props to create reusable components.</p>
+    </div>
+  );
+}
+
+export default About;
